304bd1e1e6cb4906e34057f307a919797ea7464e
sateal <128746599+sa-teal@users.noreply.github.com>
2025-09-22 10:40:09 +0200

    Node + TailwindCSS

--- functions.php
+++ functions.php
@@ -18,3 +18,24 @@ function tealblank_menus() {
     ) );
 }
 add_action( 'after_setup_theme', 'tealblank_menus' );
+
+
+
+// Encolar CSS para el frontend
+function tealblank_enqueue_assets() {
+    $style_path = get_template_directory() . '/assets/css/style.css';
+    $style_uri  = get_template_directory_uri() . '/assets/css/style.css';
+    $ver = file_exists( $style_path ) ? filemtime( $style_path ) : false;
+
+    wp_enqueue_style( 'tealblank-style', $style_uri, array(), $ver );
+}
+add_action( 'wp_enqueue_scripts', 'tealblank_enqueue_assets' );
+
+// Encolar CSS para el editor de bloques 
+function tealblank_enqueue_editor_assets() {
+    $editor_path = get_template_directory() . '/assets/css/editor.css';
+    $editor_uri  = get_template_directory_uri() . '/assets/css/editor.css';
+    $ver = file_exists( $editor_path ) ? filemtime( $editor_path ) : false;
+    wp_enqueue_style( 'tealblank-editor-style', $editor_uri, array( 'wp-edit-blocks' ), $ver );
+}
+add_action( 'enqueue_block_editor_assets', 'tealblank_enqueue_editor_assets' );
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./*.php",
+    "./inc/**/*.php",
+    "./template-parts/**/*.php",
+    "./blocks/**/*.php",
+    "./patterns/**/*.php",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
